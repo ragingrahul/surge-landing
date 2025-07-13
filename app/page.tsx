7f824e00d1e547840200c95f9cd72fd5d2ddce24
ragingrahul <raj17sarma@gmail.com>
2025-07-13 12:56:36 +0530

    git commit -m "firsr commit"

---
+++ app/page.tsx
@@ -0,0 +1,21 @@
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+// import Analytics from "./components/Analytics";
+// import Features from "./components/Features";
+// import HowItWorks from "./components/HowItWorks";
+// import Footer from "./components/Footer";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-black">
+      <Header />
+      <main>
+        <Hero />
+        {/* <Analytics /> */}
+        {/* <Features /> */}
+        {/* <HowItWorks /> */}
+      </main>
+      {/* <Footer /> */}
+    </div>
+  );
+}
